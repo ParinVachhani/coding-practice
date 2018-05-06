1ced26822170bc3cf405c2bfd464675f1c958e41
Parin Vachhani <vachhaniparin@gmail.com>
2018-05-06 19:40:51 +0530

    added first code

---
+++ hackerrank/c++/hello-world.cpp
@@ -0,0 +1,11 @@
+//Problem link - https://www.hackerrank.com/challenges/cpp-hello-world/problem
+//Code author - https://github.com/parinvachhani
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Hello, World!" << endl;
+    return 0;
+    
+}
