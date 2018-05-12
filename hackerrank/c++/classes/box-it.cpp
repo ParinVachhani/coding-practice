b5c7ad738087626874b0ecabf053aedb7a7f9c94
Parin Vachhani <vachhaniparin@gmail.com>
2018-05-12 20:28:20 +0530

    removed unnecessary comments from one code

--- hackerrank/c++/classes/box-it.cpp
+++ hackerrank/c++/classes/box-it.cpp
@@ -4,28 +4,6 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-//Implement the class Box  
-//l,b,h are integers representing the dimensions of the box
-
-// The class should have the following functions : 
-
-// Constructors: 
-// Box();
-// Box(int,int,int);
-// Box(Box);
-
-
-// int getLength(); // Return box's length
-// int getBreadth (); // Return box's breadth
-// int getHeight ();  //Return box's height
-// long long CalculateVolume(); // Return the volume of the box
-
-//Overload operator < as specified
-//bool operator<(Box& b)
-
-//Overload operator << as specified
-//ostream& operator<<(ostream& out, Box& B)
-
 class Box {
     private:
         int l,b,h;
